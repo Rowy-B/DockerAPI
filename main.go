8bc5e358bffb47f08c66db8b1633b329b8e50d69
rowyb <488796@student.fontys.nl>
2022-05-20 10:03:05 +0200

    map toegevoegd

--- main.go
+++ main.go
@@ -9,11 +9,110 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/stdcopy"
+	//"github.com/docker/docker/pkg/stdcopy"
 )
 
 func main() {
-	ListContainer()
+	imageMap := make(map[string]string) //maakt een map
+	imageMap["1"] = "alpine"
+	imageMap["2"] = "nginx"
+	var image string
+	for {
+		dialoog()
+
+		fmt.Scanln(&image)
+
+		if val, ok := imageMap[image]; ok { //checkt of image in imageMap zit
+			renDan(val)
+			lijst()
+			break
+		} else {
+			//fmt.Println("false")
+			fmt.Println("geef een getal")
+			fmt.Println("")
+			//os.Exit(0)
+
+		}
+	}
+
+	fmt.Println("goed")
+}
+
+func lijst() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	//types.Container
+	for _, container := range containers {
+		fmt.Println(container.ID)
+		fmt.Println(container.Names[0])
+		fmt.Println(container.NetworkSettings)
+		fmt.Println(container.Ports)
+	}
+}
+
+func renDan(image string) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	imageName := image
+
+	out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+	io.Copy(os.Stdout, out)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: imageName,
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp.ID)
+}
+
+func dialoog() {
+	fmt.Println("Hallo gebruiker, kies een van de volgende images die je wilt gebruiken voor je volgende container")
+	fmt.Println("1 voor alpine of 2 voor nginx")
+
+}
+
+/*func ListContainer() error {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	if len(containers) > 0 {
+		for _, container := range containers {
+			fmt.Printf("Container ID: %s", container.ID)
+		}
+	} else {
+		fmt.Println("There are no containers running")
+	}
+	return nil
 }
 
 func runContainer() {
@@ -58,41 +157,4 @@ func runContainer() {
 
 	stdcopy.StdCopy(os.Stdout, os.Stderr, out) //schrijf de logs naar het scherm
 }
-
-func lijst() {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		panic(err)
-	}
-
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, container := range containers {
-		fmt.Println(container.ID)
-	}
-}
-
-func ListContainer() error {
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		panic(err)
-	}
-
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	if len(containers) > 0 {
-		for _, container := range containers {
-			fmt.Printf("Container ID: %s", container.ID)
-		}
-	} else {
-		fmt.Println("There are no containers running")
-	}
-	return nil
-}
+*/
